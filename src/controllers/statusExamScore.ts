627aa45968fe3cd86c69550bacbee1d76169a074
Bumjargall <bumjargal.bayaraa@gmail.com>
2025-05-14 16:52:28 +0800

    exam

--- src/controllers/statusExamScore.ts
+++ src/controllers/statusExamScore.ts
@@ -1,20 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { ResultService } from "../service/resultService";
-
+import { handleError } from "src/utils/handleError";
 export const getAllResults = async (req: Request, res: Response) => {
   try {
     const results = await ResultService.getAllResults();
     res.status(200).json({ data: results });
-<<<<<<< HEAD
-  } catch (err) {
-    console.log("Алдаа: ", err);
-    res.status(500).json({ message: "Сервер шалгах..." });
-=======
     return;
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 
@@ -27,16 +21,10 @@ export const getResultByCreator = async (req: Request, res: Response) => {
       count: results.length,
       data: results,
     });
-<<<<<<< HEAD
-  } catch (err) {
-    console.log("Алдаа: ", err);
-    res.status(500).json({ message: "Сервер шалгах..." });
-=======
     return;
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 export const getResultByExamId = async (
@@ -44,18 +32,6 @@ export const getResultByExamId = async (
   res: Response
 ): Promise<void> => {
   try {
-<<<<<<< HEAD
-    const resultId = req.params.id;
-    const result = await ResultService.getResultByExamId(resultId);
-    if (!result) {
-      res.status(404).json({ message: "Шалгалтын мэдээлэл байхгүй..." });
-      return;
-    }
-    res.status(200).json({ data: result });
-  } catch (err) {
-    console.log("Алдаа: ", err);
-    res.status(500).json({ message: "Сервер алдаа гарлаа..." });
-=======
     const result = await ResultService.getResultByExamId(req.params.id);
     if (!result || result.length === 0) {
       res.status(404).json({ message: "Шалгалтын мэдээлэл олдсонгүй." });
@@ -66,7 +42,6 @@ export const getResultByExamId = async (
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 export const createResult = async (
@@ -77,18 +52,11 @@ export const createResult = async (
     const newResult = await ResultService.createResult(req.body.createResult);
     res
       .status(201)
-<<<<<<< HEAD
-      .json({ message: "Амжилттай хадгалагдлаа...", data: newResult });
-  } catch (err) {
-    console.log("Алдаа гарлаа", err);
-    res.status(500).json({ message: "Сервер алдаа гарлаа..." });
-=======
       .json({ message: "Амжилттай хадгалагдлаа", data: newResult });
     return;
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 export const updateResult = async (
@@ -101,26 +69,15 @@ export const updateResult = async (
       req.body
     );
     if (matchedCount === 0) {
-<<<<<<< HEAD
-      console.warn("Өөрчлөлт хийгдээгүй байна.");
-      res.status(404).json({ message: "Мэдээлэл олдсонгүй..." });
-=======
       res.status(404).json({ message: "Мэдээлэл олдсонгүй." });
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
       return;
     }
 
     res.status(200).json({ data: result });
-<<<<<<< HEAD
-  } catch (err) {
-    console.log("Алдаа: ", err);
-    res.status(500).json({ message: "Сервер алдааг шалгах ..." });
-=======
     return;
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 
@@ -132,23 +89,14 @@ export const deleteResult = async (
     const { deletedCount } = await ResultService.deleteResult(req.params.id);
 
     if (deletedCount === 0) {
-<<<<<<< HEAD
-      res.status(404).json({ message: "Мэдээлэл олдсонгүй..." });
-=======
       res.status(404).json({ message: "Мэдээлэл олдсонгүй." });
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
       return;
     }
 
     res.status(200).json({ message: "Устгагдлаа..." });
   } catch (err) {
-<<<<<<< HEAD
-    console.log("Алдаа: ", err);
-    res.status(500).json({ message: "Сервер алдаа..." });
-=======
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 
@@ -160,17 +108,10 @@ export const getExamsWithSubmissions = async (
   try {
     const exams = await ResultService.getExamsWithSubmissions();
     res.status(200).json({ success: true, count: exams.length, data: exams });
-<<<<<<< HEAD
-  } catch (err) {
-    console.error("Алдаа: ", err);
-    res.status(500).json({ message: "Серверийн алдаа гарлаа" });
-    next(err);
-=======
     return;
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 export const getResultByUsers = async (
@@ -179,19 +120,6 @@ export const getResultByUsers = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-<<<<<<< HEAD
-    const { examId } = req.params;
-    const result = await ResultService.getResultByUsers(examId);
-    if (!result) {
-      res.status(404).json({ message: "Шалгалтын мэдээлэл байхгүй..." });
-      return;
-    }
-    res.status(200).json({ success: true, count: result.length, data: result });
-  } catch (err) {
-    console.log("Алдаа: ", err);
-    res.status(500).json({ message: "Сервер алдаа гарлаа..." });
-    next(err);
-=======
     const result = await ResultService.getResultByUsers(req.params.examId);
     if (!result || result.length === 0) {
       res.status(404).json({ message: "Шалгалтын мэдээлэл олдсонгүй." });
@@ -202,7 +130,6 @@ export const getResultByUsers = async (
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 export const getResultByUserId = async (
@@ -210,62 +137,36 @@ export const getResultByUserId = async (
   res: Response
 ): Promise<void> => {
   try {
-<<<<<<< HEAD
-    const { userId } = req.params;
-    const result = await ResultService.getResultByUserId(userId);
-    if (!result) {
-      res.status(404).json({ message: "Шалгалтын мэдээлэл байхгүй..." });
-=======
     const result = await ResultService.getResultByUserId(req.params.userId);
     if (!result || result.length === 0) {
       res.status(404).json({ message: "Хэрэглэгчийн шалгалт олдсонгүй." });
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
       return;
     }
     //console.log("result-----+++++++>", result);
     res.status(200).json({ success: true, count: result.length, data: result });
-<<<<<<< HEAD
-  } catch (err) {
-    console.log("Алдаа: ", err);
-    res.status(500).json({ message: "Сервер алдаа гарлаа..." });
-=======
     return;
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 
 export const deleteResultByExamIdByUserId = async (
   req: Request,
   res: Response
-<<<<<<< HEAD
-): Promise<void> => {
-  try {
-    const { studentId, examId } = req.params;
-=======
 ) => {
   try {
     const { examId, studentId } = req.params;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
     const { deletedCount } = await ResultService.deleteResultByExamIdByUserId(
       examId,
       studentId
     );
     if (deletedCount === 0) {
-<<<<<<< HEAD
-      res.status(404).json({ message: "Мэдээлэл олдсонгүй..." });
-      return;
-    }
-    res.status(200).json({ message: "Амжилттай устгагдлаа..." });
-=======
       res.status(404).json({ message: "Мэдээлэл олдсонгүй." });
       return;
     }
     res.status(200).json({ message: "Амжилттай устгагдлаа." });
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   } catch (err) {
     console.log("Алдаа: ", err);
     res.status(500).json({ message: "Сервер алдаа..." });
@@ -280,23 +181,11 @@ export const checkResultByExamUser = async (
   try {
     const { studentId, examId } = req.params;
     const status = await ResultService.checkResultByExamUser(examId, studentId);
-<<<<<<< HEAD
-
-    res.status(200).json({
-      success: true,
-      status, // ✅ Одоо status хувьсагч тодорхойлогдсон
-    });
-  } catch (err) {
-    console.log("Алдаа: ", err);
-    res.status(500).json({ message: "Сервер алдаа..." });
-    next(err);
-=======
     res.status(200).json({ success: true, status });
     return;
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 export const getExamTakenCount = async (
@@ -307,22 +196,14 @@ export const getExamTakenCount = async (
   try {
     const count = await ResultService.getExamTakenCount();
     res.status(200).json({ success: true, count });
-<<<<<<< HEAD
-  } catch (error) {
-    next(error);
-=======
     return;
   } catch (err) {
     handleError(res, err);
     return;
->>>>>>> 107c55b961046237b10ae4c5bd0bbbd2cda4e9b4
   }
 };
 
-export const getExamTakenPerMonth = async (
-  req: Request,
-  res: Response
-)=> {
+export const getExamTakenPerMonth = async (req: Request, res: Response) => {
   try {
     const data = await ResultService.getExamTakenPerMonth();
 
@@ -348,13 +229,10 @@ export const getExamTakenPerMonth = async (
     console.log("-----Controller---->", formatted);
     res.status(200).json({ success: true, data: formatted });
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        success: false,
-        message: "Ашиглагдсан шалгалтын дата авахад алдаа гарлаа",
-       error: err
-      });
+    res.status(500).json({
+      success: false,
+      message: "Ашиглагдсан шалгалтын дата авахад алдаа гарлаа",
+      error: err,
+    });
   }
 };
-
